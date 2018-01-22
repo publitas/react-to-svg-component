4f7a200be704bb8673b340a932d6305fe5a5bf9f
Felix Hageloh <felix.hageloh@gmail.com>
2018-01-22 13:44:12 +0100

    Add default props

--- src/toComponent.js
+++ src/toComponent.js
@@ -5,13 +5,18 @@ module.exports = function toReactComponent(svgString, name) {
 };
 
 function convert(renderFunction, name) {
+  const n = name || 'Svg';
   return `
     const React = require('react');
-    module.exports = function ${name || ''}(props) {
+    module.exports = function ${n}(props) {
       const newProps = Object.assign({}, props);
       delete newProps.color;
       newProps.fill = (typeof props.color !== 'function') ? props.color : null;
       return ${renderFunction}(newProps);
+    };
+    ${n}.defaultProps = {
+      color: (_, original) => original,
+      preserveAspectRatio: 'xMidYMid meet',
     };
   `;
 }
