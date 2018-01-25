0c025ab7f38ba0253cd91097625f7c26b04d6604
Vladimir Grubor <grubor@gmail.com>
2018-01-25 11:55:44 +0100

    Don't replace color value if value is "none"
    If svg defined fill/stroke as none we should not change that.

--- src/parameterizeColors.js
+++ src/parameterizeColors.js
@@ -7,6 +7,7 @@ module.exports = function parameterizeColors(path) {
   if (!node.name || colorAttrs.indexOf(node.name.name) === -1) return;
   const name = node.name.name;
   const value = node.value.value;
+  if (value === 'none') return;
   path.get('value').replaceWith(
     t.jSXExpressionContainer(
       parseExpression(`evalColor('${name}', '${value}', props.color)`)
